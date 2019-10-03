28ccba5c2c3d267e2f30d4b3f36fa874fcdc6e58
Marcos Raudkett <teamjws@hotmail.com>
2019-10-03 15:04:20 +0300

    Updates
    Updated UI & UX.

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem, copyArrayItem, CdkDrag} from '@angular/cdk/drag-drop';
 import {ItemComponent} from '../../interfaces/item-component';
+import {ElementDatabaseService} from 'src/app/services/element-database.service'
+import {cloneDeep} from 'lodash';
 
 @Component({
   selector: 'app-main-view',
@@ -9,157 +11,88 @@ import {ItemComponent} from '../../interfaces/item-component';
 })
 export class MainViewComponent implements OnInit {
   components: object[];
+  component : object;
   todo: string[];
-  board: ItemComponent[];
   template ="Template";
   boardMain = "Board";
   componentsTitle = "Components";
   itemPrompt: string;
   idForBoard: number;
+  i:number;
+  idList=[];
 
+  
 
-
-  constructor() { }
+  constructor(private elementDataBase: ElementDatabaseService) { }
 
   ngOnInit() {
-  
-    this.components = [
-    {
-      'id':1,
-      'title':'Title',
-      'prompt':'',
-      'width':4,
-      'colwidths':'',
-      'main':true,
-      'editing':false
-    },
-    {
-      'id':2,
-      'title':'Field',
-      'type':'header',
-      'prompt':'',
-      'default':'',
-      'row':1,
-      'colspan':2,
-      'halign':'center',
-      'editing':false
-    },
-    {
-      'id':3,
-      'title':'Text',
-      'type':'textbox',
-      'prompt':'',
-      'default':'',
-      'row':1,
-      'colspan':2,
-      'editing':false
-    },
-    {
-      'id':4,
-      'title':'ComboBox',
-      'type':'combobox',
-      'prompt':'',
-      'default': [{
-        'type':'combo',
-        'id':'',
-        'prompt':''
-      },{
-        'type':'combo',
-        'id':'',
-        'prompt':''
-      },{
-        'type':'combo',
-        'id':'',
-        'prompt':''
-      }]
-      ,
-      'row':1,
-      'colspan':2,
-      'halign':'left',
-      'editing':false
-    },
-    {
-      'id':5,
-      'title':'Radio',
-      'type':'prompt',
-      'prompt':'',
-      'default':[{
-        'type':'radio',
-        'id':'',
-        'prompt':''
-      },{
-        'type':'radio',
-        'id':'',
-        'prompt':''
-      },{
-        'type':'radio',
-        'id':'',
-        'prompt':''
-      }],
-      'row':1,
-      'colspan':2,
-      'editing':false
-    },
-    {
-      'id':6,
-      'title':'Button',
-      'type':'submit',
-      'prompt':'',
-      'default':'',
-      'row':1,
-      'width':5,
-      'colspan':2,
-      'hiddenInMobile':true,
-      'editing':false
-    }
-  ];
-  this.todo = [
-    'Get to work',
-    'Pick up groceries',
-    'Go home',
-    'Fall asleep'
-  ];
-
-  this.board = [
     
-  ];
-  
+    this.getElements();
+
   }
+
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      copyArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
-      
+    } 
+    else {
+            console.log(" board: ",this.board);
+            const clone = cloneDeep(event.previousContainer.data[event.previousIndex]);
+            clone['id']=Math.floor(Math.random() * Math.floor(20000));;
+            this.checkId(clone);
+                  
+            
+
+    // Add the clone to the new array.
+           event.container.data.splice(event.currentIndex, 0, clone);
       
     }
+  
   }
   noReturnPredicate() {
     return false;
   }
-  addItem(itemPrompt: string): void{
 
-    this.board.push({
-      
-        'id':1,
-        'title':'Title',
-        'prompt':this.itemPrompt,
-        'width':4,
-        'colwidths':'',
-        'main':true,
-        'editing':false
-      
-    })
-  }
   
+  board = [
+    
+  ]; 
+
+  
+
+
   editItem(item:ItemComponent): void{
+    
     item.editing=true;
+    
+   
   }
+
   deleteItem(id:number){
     console.log("deleting id: "+id);
     this.board = this.board.filter(board=>board.id!==id);
+    delete this.idList[this.board[id]];
+   
+  }
+
+  getElements(): void {
+    this.components=this.elementDataBase.getElements();
+    //console.log(this.components +" components");
+  }
+
+ 
+
+  checkId(clone:any): void{
+    if(this.idList.includes(clone['id'])){
+      clone['id']= Math.floor(Math.random()*Math.floor(2000));
+     
+     
+    }
+    if(this.idList.includes(clone['id']))
+    this.checkId(clone);
+    
+    this.idList.push(clone['id']);
+    
+    
   }
 }
